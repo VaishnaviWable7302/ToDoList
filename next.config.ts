6aae652916c088da8ff4d4243f51a86e7877ad20
VaishnaviWable7302 <vaishnaviwable2020@gmail.com>
2025-02-08 17:56:27 +0530

    ToDoList

--- next.config.ts
+++ next.config.ts
@@ -3,8 +3,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   output: "export", // Enables static export
-  basePath: "ToDoList", // Replace with your GitHub repo name
-  assetPrefix: "ToDoList",
+  basePath: "/ToDoList", // Replace with your GitHub repo name
+  assetPrefix: "/ToDoList",
 };
 
 export default nextConfig;
